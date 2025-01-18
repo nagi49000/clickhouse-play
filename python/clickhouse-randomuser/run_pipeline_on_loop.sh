253c7f3504f1582371d61fdddf5029d10aad28a6
nagi <nagi>
2025-01-18 17:42:39 +0000

    wrapped pipeline in dockerfile

--- python/clickhouse-randomuser/run_pipeline_on_loop.sh
+++ python/clickhouse-randomuser/run_pipeline_on_loop.sh
@@ -9,10 +9,11 @@ cd src
 # set env vars to defaults if not already set
 N_RECORD=${N_RECORD:-50}
 SLEEP_SECS=${SLEEP_SECS:-30}
+LUIGI_WORKDIR=${LUIGI_WORKDIR:-luigi-output}
 
 # loop away
 while true
 do
-  python -m luigi --module clickhouse_randomuser.luigi_pipeline  SchemaedCsvRows --workdir luigi-output --local-scheduler --DownloadRandomUsers-n-record ${N_RECORD}
+  python -m luigi --module clickhouse_randomuser.luigi_pipeline  SchemaedCsvRows --workdir ${LUIGI_WORKDIR} --local-scheduler --DownloadRandomUsers-n-record ${N_RECORD}
   sleep ${SLEEP_SECS}
 done
